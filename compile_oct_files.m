031e4ad60d267b49e5c3de0a1768fc97344b198d
Schinzilord <schinzilord@octarisk.com>
2018-01-20 22:39:08 +0100

    * additional help text

--- compile_oct_files.m
+++ compile_oct_files.m
@@ -1,3 +1,23 @@
+%# Copyright (C) 2017 Stefan Schloegl <schinzilord@octarisk.com>
+%#
+%# This program is free software; you can redistribute it and/or modify it under
+%# the terms of the GNU General Public License as published by the Free Software
+%# Foundation; either version 3 of the License, or (at your option) any later
+%# version.
+%#
+%# This program is distributed in the hope that it will be useful, but WITHOUT
+%# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+%# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+%# details.
+
+%# -*- texinfo -*-
+%# @deftypefn {Function File} {} compile_oct_files (@var{path_octarisk})
+%#
+%# Compile all .cc files in folder  @var{path_octarisk}/oct_files and move
+%# successfully compiled .oct files to top folder. 
+%#
+%# @end deftypefn
+
 function compile_oct_files(path_octarisk)
 % it is assumed that all oct files are in subfolder /oct_files
 current_dir = pwd;
